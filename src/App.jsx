4f2b9f225b61847a272bb5f73ffd6488d868722a
Yorch82 <jcampo1982@outlook.com>
2023-08-31 12:44:13 +0200

    pointer

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 import { Ground } from './components/Ground';
-import { FPV as Fpv} from './components/FPV';
+import { FPV as Fpv } from './components/FPV';
 import { Player } from './components/Player';
 
 function App() {
   return (
-    <Canvas>
-      <Sky sunPosition={[100, 100, 20]} />
-      <ambientLight intensity={0.5} />
-      <Fpv />
-      <Physics>
-        <Player />
-        <Ground />
-      </Physics>
-    </Canvas>
+    <>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Fpv />
+        <Physics>
+          <Player />
+          <Ground />
+        </Physics>
+      </Canvas>
+      <div className="pointer">+</div>
+    </>
   );
 }
 

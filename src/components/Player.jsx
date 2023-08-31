4143efd9648d5ad62a57e26d78b783f897266345
Yorch82 <jcampo1982@outlook.com>
2023-08-31 12:41:06 +0200

    movimiento player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,14 +2,26 @@ import { useSphere } from "@react-three/cannon";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard";
+
+const CHARACTER_SPEED = 4;
+const CHARACTER_JUMP_FORCE = 4;
 
 export const Player = () => {
+    const {
+        moveBackward, 
+        moveForward, 
+        moveLeft, 
+        moveRight,
+        jump, 
+    } = useKeyboard();
+
     const { camera } = useThree();
     const [ ref, api ] = useSphere(() => ({
         mass: 1,
         type: "Dynamic",
         position: [0, 1, 0]
-    }))
+    }));
 
     const pos = useRef([0, 0, 0]);
     useEffect(() => {
@@ -34,7 +46,40 @@ export const Player = () => {
             )
         )
 
-        api.velocity.set(0, 0, -1);
+        const direction = new Vector3();
+
+        const frontVector = new Vector3(
+            0,
+            0,
+            (moveBackward ? 1 : 0) - (moveForward ? 1 :0)
+        );
+
+        const sideVector = new Vector3(
+            (moveLeft ? 1 : 0) - (moveRight ? 1 :0),
+            0,
+            0
+        );
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(CHARACTER_SPEED)
+            .applyEuler(camera.rotation)
+
+        api.velocity.set(
+            direction.x,
+            vel.current[1],
+            direction.z
+        )
+
+        if (jump && Math.abs(vel.current[1]) < 0.05) {
+            api.velocity.set(
+                vel.current[0],
+                CHARACTER_JUMP_FORCE,
+                vel.current[2]
+            )
+        }
+      
     });
 
     return (
